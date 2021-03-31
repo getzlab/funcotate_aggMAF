6597dd7708c8be19bf7af3dcbbe999b9987a2366
Qing Zhang <qing@broadinstitute.org>
2021-03-31 22:40:18 +0000

    stage left alignment fix

--- src/merge.py
+++ src/merge.py
@@ -3,19 +3,29 @@ import pandas as pd
 
 # parse inputs
 def parse_args():
-	parser = argparse.ArgumentParser(description = "merge funcotated MAF")
+    parser = argparse.ArgumentParser(description="merge funcotated MAF")
 
-	# files IO
-	parser.add_argument("-i", required = True, help = "Paths to input mafs concatenated by comma")
-	parser.add_argument("-o", required = True, help = "Path to output maf file (with a minimal set of columns)")
-	parser.add_argument("-r", help = "Path to columns to remove", default = None)
-	args = parser.parse_args()
-	return args
+    # files IO
+    parser.add_argument(
+        "-i", required=True, help="Paths to input mafs concatenated by comma"
+    )
+    parser.add_argument(
+        "-o",
+        required=True,
+        help="Path to output maf file (with a minimal set of columns)",
+    )
+    parser.add_argument("-r", help="Path to columns to remove", default=None)
+    parser.add_argument(
+        "-skip", help="Number of noninformative rows", default=None, type=int
+    )
+    args = parser.parse_args()
+    return args
+
 
 args = parse_args()
 
 with open(args.i) as f:
-	inputs = f.read()
+    inputs = f.read()
 
 
 l = []
@@ -25,6 +35,14 @@ with open(args.r) as f:
     cols_to_remove = [line.rstrip() for line in f]
 
 for maf in inputs.split(","):
-	l.append(pd.read_csv(maf, sep = '\t', usecols= lambda x: x not in cols_to_remove, skiprows= lambda x: x in list(range(11)), low_memory=False))
+    l.append(
+        pd.read_csv(
+            maf,
+            sep="\t",
+            usecols=lambda x: x not in cols_to_remove,
+            skiprows=lambda x: x in list(range(args.skip)),
+            low_memory=False,
+        )
+    )
 
-pd.concat(l).to_csv(args.o, index = False, sep = '\t')
+pd.concat(l).to_csv(args.o, index=False, sep="\t")
--- wolF/tasks.py
+++ wolF/tasks.py
@@ -1,67 +1,71 @@
 from wolf import Task
 import os
 
-img = "gcr.io/broad-getzlab-workflows/maf2vcflite:v5"
+img = "gcr.io/broad-getzlab-workflows/maf2vcflite:v7"
 
-def split_maf(
-  fullmaf
-):
 
-  return Task(
-      name = "split_maf",
-      inputs = {
-          "maf" : fullmaf
-      },
-      outputs = {
-          "submaf" : "*.submaf"
-      },
-      script = [
-          """
+def split_maf(fullmaf):
+
+    return Task(
+        name="split_maf",
+        inputs={"maf": fullmaf},
+        outputs={"submaf": "*.submaf"},
+        script=[
+            """
           python /app/subset_maf.py ${maf}
           """
-      ],
-      docker = img,
-      resources = { "mem" : "10G" }
+        ],
+        docker=img,
+        resources={"mem": "10G"},
     )
 
-def maf2vcf(
-  ref,
-  maf
-):
+
+def maf2vcf(ref, maf):
     # preprocessing logic can go here
     return Task(
-      name = "maf2vcf",
-      inputs = {
-        "ref" : ref,
-        "maf" : maf,
-      },
-      script = [
-        """
+        name="maf2vcf",
+        inputs={
+            "ref": ref,
+            "maf": maf,
+        },
+        script=[
+            """
         pairname=$(basename ${maf} .submaf)
         python /app/write_vcf.py -tsv ${maf} -ref ${ref} -o_vcf ${pairname}.vcf
         """
-      ],
-      outputs = {
-        "vcf" : "*.vcf"
-      },
-      docker = img,
-      resources = { "mem" : "2G" },
+        ],
+        outputs={"vcf": "*.vcf"},
+        docker=img,
+        resources={"mem": "2G"},
     )
 
 
-def funcotate(
-  vcf,
-  ref
-):
+def left_align(vcf, ref):
+    return Task(
+        name="gatk_leftalign",
+        inputs={"ref": ref, "vcf": vcf},
+        script=[
+            """
+pairname=$(basename ${vcf} .vcf)
+gatk LeftAlignAndTrimVariants -R ${ref} -V ${vcf} -O ${pairname}.fixed.vcf
+      """
+        ],
+        outputs={"fixed_vcf": "*.fixed.vcf"},
+        docker="gcr.io/broad-getzlab-workflows/gatk4_wolf:v7",
+        resources={"mem": "2G"},
+    )
+
+
+def funcotate(vcf, ref):
     # preprocessing logic can go here
     return Task(
-      name = "funcotate",
-      inputs = {
-        "ref" : ref,
-        "vcf" : vcf,
-      },
-      script = [
-        """
+        name="funcotate",
+        inputs={
+            "ref": ref,
+            "vcf": vcf,
+        },
+        script=[
+            """
         pairname=$(basename ${vcf} .vcf)
         echo ${pairname}
         gatk Funcotator \
@@ -71,33 +75,28 @@ def funcotate(
     --output-file-format MAF \
     --data-sources-path /mnt/nfs/wgs_ref/funcotator_dataSources.v1.6.20190124s \
     --transcript-list /mnt/nfs/wgs_ref/transcriptList.exact_uniprot_matches.AKT1_CRLF2_FGFR1.txt \
-    --disable-sequence-dictionary-validation \
     --annotation-default normal_barcode:${pairname}_N \
     --annotation-default tumor_barcode:${pairname}_T \
     --ref-version hg38
         """
-      ],
-      outputs = {
-        "maf" : "*.maf"
-      },
-      docker = "gcr.io/broad-getzlab-workflows/gatk4_wolf:v6",
-      resources = { "mem" : "6G" },
+        ],
+        outputs={"maf": "*.maf"},
+        docker="gcr.io/broad-getzlab-workflows/gatk4_wolf:v6",
+        resources={"mem": "6G"},
     )
 
 
 def merge(mafs_in, rcols):
-  return Task(
-    name = 'merge_funcotated',
-    inputs = {
-      "mafs" : mafs_in,
-      "rcols" : rcols
-    },
-    script = [
+    return Task(
+        name="merge_funcotated",
+        inputs={"mafs": mafs_in, "rcols": rcols},
+        script=[
+            """
+skiplines=$(grep '##' out.maf | wc -l)
+python /app/merge.py -i <(head -c-1 ${mafs} | tr '\n' ',') -o merged_final.maf -r ${rcols} -skip $(( $skiplines + 1 ))
       """
-python /app/merge.py -i <(head -c-1 ${mafs} | tr '\n' ',') -o merged_final.maf -r ${rcols}
-      """
-    ],
-    outputs= {"final" : "merged_final.maf"},
-    docker = "gcr.io/broad-getzlab-workflows/maf2vcflite:v6",
-    resources = { "mem" : "10G" },
-  )
+        ],
+        outputs={"final": "merged_final.maf"},
+        docker="gcr.io/broad-getzlab-workflows/maf2vcflite:v6",
+        resources={"mem": "10G"},
+    )
